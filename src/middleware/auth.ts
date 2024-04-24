611017d400dd8d0e580c75452933a977c0388673
Niklas Jacobsen <niklasjacobsen4201@gmail.com>
2024-04-24 13:04:58 +0200

    add user authentication

---
+++ src/middleware/auth.ts
@@ -0,0 +1,36 @@
+import { Secret, JwtPayload } from 'jsonwebtoken';
+import '../data-source';
+import { Request, Response, NextFunction } from 'express';
+import { AuthController } from '../controller/AuthController';
+
+export const SECRET_KEY: Secret = process.env.SECRET_KEY;
+
+export interface CustomRequest extends Request {
+  token: string | JwtPayload;
+}
+
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
+  const authController = new AuthController();
+  try {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+
+    if (!token) {
+      throw new Error();
+    }
+
+    const decoded = await authController.verifyJWT(token);
+    if (!decoded) {
+      throw new Error();
+    }
+
+    res.locals.userId = decoded;
+    res.locals.token = token;
+
+    next();
+  } catch (err) {
+    res.status(401).json({
+      status: 'error',
+      message: 'Could not authenticate',
+    });
+  }
+};
